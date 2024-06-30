28adc25f048a60307169ecbcb68806d454a206ae
Acacio de Lima <limadeacacio@gmail.com>
2024-06-30 00:43:00 -0300

    updated SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,10 +15,24 @@ export const metadata: Metadata = {
   keywords:
     "horário de beber, copão, cerveja, vodka, whisky, gin, drink, bar, boteco, balada, festa, happy hour, beber, beba com moderação, beba responsavelmente, beba com responsa, beba com responsabilidade",
   openGraph: {
+    siteName: "Jah pode tomar copão?",
     title: "Jah pode tomar copão?",
     description: "Será que já chegou a hora de abrir uma latinha? Descubra",
+    url: "https://jah-pod-tomar-copao.vercel.app/",
+    images: [
+      {
+        url: "drink.png",
+        width: 512,
+        height: 512,
+        alt: "drink img",
+      },
+    ],
+    locale: "pt_BR",
     type: "website",
-    images: [{ url: "drink.png", width: 512, height: 512, alt: "drink img" }],
+  },
+  robots: {
+    index: true,
+    follow: true,
   },
 };
 
